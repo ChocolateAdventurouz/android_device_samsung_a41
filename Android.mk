d810e87185b9dff8094e3abd9f33d794692d9b32
ChocolateAdventurouz <aggeloskarapatakis@hotmail.com>
2023-12-03 23:35:18 +0200

    Added device tree source

---
+++ Android.mk
@@ -0,0 +1,4 @@
+ifeq ($(TARGET_DEVICE),a41)
+  subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
+  $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
+endif
---
+++ device.mk
@@ -0,0 +1,26 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from mt6768-common
+$(call inherit-product, device/samsung/mt6768-common/mt6768.mk)
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+
+# AAPT
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
+# Soong
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# Shipping API Level
+PRODUCT_SHIPPING_API_LEVEL := 29
+
+# Inherit the proprietary files
+$(call inherit-product, vendor/samsung/a41/a41-vendor.mk)
